00a6575ff9de5d1113110961c4e2d862e3a905ca
schapman1974 <schapman1974@gmail.com>
2016-05-03 22:20:46 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'tinymongo',
+    packages = ['tinymongo'],
+    version = '0.1',
+    description = 'A flat file drop in replacement for mongodb.  Requires Tinydb',
+    author = 'Stephen Chapman',
+    author_email = 'schapman1974@gmail.com',
+    url = 'https://github.com/schapman1974/tinymongo',
+    download_url = 'https://github.com/schapman1974/tinymongo/archive/master.zip',
+    keywords = ['mongodb', 'drop-in', 'database', 'tinydb'],
+    classifiers = [],
+)
