09ee767bbf200a0da8b9edecd5670d2e5a0ef2a7
jjones6 <jjones6@moog.com>
2016-11-11 09:53:44 -0500

    Improved demo by removing/rewriting the database.

--- tinymongo/demo.py
+++ tinymongo/demo.py
@@ -1,13 +1,20 @@
 import tinymongo as tm
 import os
 
+db_name = 'demo_db'
 try:
-    os.remove("tinydb/pacemain.json")
-    os.remove("tinydb/pacelog.json")
-except:
+    for f in os.listdir(os.path.join('.', db_name)):
+        os.remove(os.path.join(db_name, f))
+except FileNotFoundError:
     pass
+
+try:
+    os.rmdir('demo_db')
+except FileNotFoundError:
+    pass
+
 # you can include a folder name as a parameter if not it will default to "tinydb"
-tinyClient = tm.TinyMongoClient()
+tinyClient = tm.TinyMongoClient(db_name)
 
 # either creates a new database file or accesses an existing one
 tinyDatabase = tinyClient.tinyDatabase
@@ -18,6 +25,12 @@ tinyCollection = tinyDatabase.tinyCollection
 # insert data adds a new record returns _id
 recordId = tinyCollection.insert({"username": "admin", "password": "admin", "module": "somemodule"})
 userInfo = tinyCollection.find_one({"_id": recordId})  # returns the record inserted
+print('find one result: '.format())
+
+#cursor = tinyCollection.find({})
+#for c in cursor:
+#    print(c['username'])
 
 # update data returns boolean if successful
 upd = tinyCollection.update({"username": "admin"}, {"$set": {"module": "someothermodule"}})
+
