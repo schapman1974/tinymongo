9662a2a5d451128444a030ee645a1b9130969b07
schapman1974 <schapman1974@gmail.com>
2017-03-17 14:00:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import setup, find_packages
 
-long_description = open('README.md').read()
+def read(*names, **kwargs):
+    """Read a file."""
+    return io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+        ).read()
+
+def parse_md_to_rst(file):
+    """Read Markdown file and convert to ReStructured Text."""
+    try:
+        from m2r import parse_from_file
+        return(parse_from_file(file).replace("artwork/","http://198.27.119.65/"))
+    except ImportError:
+        # m2r may not be installed in user environment
+        return(read(file))
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -15,7 +29,6 @@ setup(
     url='https://github.com/schapman1974/tinymongo',
     download_url='https://github.com/schapman1974/tinymongo/archive/master.zip',
     keywords=['mongodb', 'drop-in', 'database', 'tinydb'],
-    long_description = long_description,
+    long_description = parse_md_to_rst("README.md"),
     classifiers=[],
     install_requires=requirements
-)
